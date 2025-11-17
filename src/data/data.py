4a310c54652d35d466d3a41c35f6af3954721a75
Frederik Holbech <faho@itu.dk>
2025-11-17 12:50:36 +0100

    ci: adding utils and data scripts style: Changes to CCDS and DVC structure

---
+++ src/data/data.py
@@ -0,0 +1,149 @@
+import pandas as pd
+import os
+import warnings
+import datetime
+import json
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import joblib
+import subprocess
+
+from utils import describe_numeric_col, impute_missing_values
+
+#Dates
+max_date = "2024-01-31"
+min_date = "2024-01-01"
+
+#Create the artifacts directory if it doesn't exist
+os.makedirs("artifacts",exist_ok=True)
+
+#Suppress warnings and set pandas display options
+warnings.filterwarnings('ignore')
+pd.set_option('display.float_format',lambda x: "%.3f" % x)
+
+#Pull data from DVC in the root of the project
+def dvc_pull():
+    PROJECT_ROOT = r"C:\Users\Frederik\MLOps_2025_Project"
+
+    subprocess.run(
+        ["dvc", "pull"],
+        cwd=PROJECT_ROOT,
+        check=True
+    )
+
+dvc_pull()
+
+
+data = pd.read_csv("raw_data.csv")
+
+#Making sure date limits are set correctly
+if not max_date:
+    max_date = pd.to_datetime(datetime.datetime.now().date()).date()
+else:
+    max_date = pd.to_datetime(max_date).date()
+
+min_date = pd.to_datetime(min_date).date()
+
+# Time limit data
+data["date_part"] = pd.to_datetime(data["date_part"]).dt.date
+data = data[(data["date_part"] >= min_date) & (data["date_part"] <= max_date)]
+
+min_date = data["date_part"].min()
+max_date = data["date_part"].max()
+date_limits = {"min_date": str(min_date), "max_date": str(max_date)}
+with open("./artifacts/date_limits.json", "w") as f:
+    json.dump(date_limits, f)
+
+#Not all columns are relevant for modelling
+data = data.drop(
+    [
+        "is_active", "marketing_consent", "first_booking", "existing_customer", "last_seen"
+    ],
+    axis=1)
+
+#Removing columns that will be added back after the EDA
+data = data.drop(
+    ["domain", "country", "visited_learn_more_before_booking", "visited_faq"],
+    axis=1
+)
+
+#Remove rows with empty target variable
+#Remove rows with other invalid column data
+
+data["lead_indicator"].replace("", np.nan, inplace=True)
+data["lead_id"].replace("", np.nan, inplace=True)
+data["customer_code"].replace("", np.nan, inplace=True)
+
+data = data.dropna(axis=0, subset=["lead_indicator"])
+data = data.dropna(axis=0, subset=["lead_id"])
+
+data = data[data.source == "signup"]
+result=data.lead_indicator.value_counts(normalize = True)
+
+#Create categorical data columns
+vars = [
+    "lead_id", "lead_indicator", "customer_group", "onboarding", "source", "customer_code"
+]
+
+for col in vars:
+    data[col] = data[col].astype("object")
+
+#Separate categorical and continuous columns
+cont_vars = data.loc[:, ((data.dtypes=="float64")|(data.dtypes=="int64"))]
+cat_vars = data.loc[:, (data.dtypes=="object")]
+
+#Outliers, finding the outliers using 2 standard deviations from the mean
+cont_vars = cont_vars.apply(lambda x: x.clip(lower = (x.mean()-2*x.std()),
+                                             upper = (x.mean()+2*x.std())))
+outlier_summary = cont_vars.apply(describe_numeric_col).T
+outlier_summary.to_csv('./artifacts/outlier_summary.csv')
+
+#Impute missing values
+cat_missing_impute = cat_vars.mode(numeric_only=False, dropna=True)
+cat_missing_impute.to_csv("./artifacts/cat_missing_impute.csv")
+
+#Continuous variables missing values
+cont_vars = cont_vars.apply(impute_missing_values)
+cont_vars.apply(describe_numeric_col).T
+
+#Categorical variables missing values
+cat_vars.loc[cat_vars['customer_code'].isna(),'customer_code'] = 'None'
+cat_vars = cat_vars.apply(impute_missing_values)
+cat_vars.apply(lambda x: pd.Series([x.count(), x.isnull().sum()], index = ['Count', 'Missing'])).T
+
+#Data standardization
+scaler_path = "./artifacts/scaler.pkl"
+
+scaler = MinMaxScaler()
+scaler.fit(cont_vars)
+
+joblib.dump(value=scaler, filename=scaler_path)
+
+cont_vars = pd.DataFrame(scaler.transform(cont_vars), columns=cont_vars.columns)
+
+#Combine categorical and continuous variables back together
+cont_vars = cont_vars.reset_index(drop=True)
+cat_vars = cat_vars.reset_index(drop=True)
+data = pd.concat([cat_vars, cont_vars], axis=1)
+
+#Data drift artifact ???
+data_columns = list(data.columns)
+with open('./artifacts/columns_drift.json','w+') as f:           
+    json.dump(data_columns,f)
+    
+data.to_csv('./artifacts/training_data.csv', index=False)
+
+#Binning together the categorical variables
+data['bin_source'] = data['source']
+values_list = ['li', 'organic','signup','fb']
+data.loc[~data['source'].isin(values_list),'bin_source'] = 'Others'
+mapping = {'li' : 'socials', 
+           'fb' : 'socials', 
+           'organic': 'group1', 
+           'signup': 'group1'
+           }
+
+data['bin_source'] = data['source'].map(mapping)
+
+#Save the golden training data
+data.to_csv('./artifacts/train_data_gold.csv', index=False)
