742933b267baebd3910874a4daaca05ccba21bde
Frederik Holbech <faho@itu.dk>
2025-11-24 11:42:16 +0100

    feat: Added Evaluation of models and selection of best model for deployment

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -3,6 +3,7 @@ import mlflow
 from utils import wait_until_ready
 import json
 import pandas as pd
+from mlflow.tracking import MlflowClient
 
 # Constants used
 current_date = datetime.datetime.now().strftime("%Y_%B_%d")
@@ -27,3 +28,41 @@ results_df = pd.DataFrame({model: val["weighted avg"] for model, val in model_re
 
 # Selecting the best model based on f1-score
 best_model = results_df.sort_values("f1-score", ascending=False).iloc[0].name
+
+# Get production model details
+client = MlflowClient()
+prod_model = [model for model in client.search_model_versions(f"name='{model_name}'") if dict(model)['current_stage']=='Production']
+prod_model_exists = len(prod_model)>0
+
+if prod_model_exists:
+    prod_model_version = dict(prod_model[0])['version']
+    prod_model_run_id = dict(prod_model[0])['run_id']
+
+# Comparing newly trained best model with production model
+train_model_score = experiment_best["metrics.f1_score"]
+model_details = {}
+model_status = {}
+run_id = None
+
+if prod_model_exists:
+    data, details = mlflow.get_run(prod_model_run_id)
+    prod_model_score = data[1]["metrics.f1_score"]
+
+    model_status["current"] = train_model_score
+    model_status["prod"] = prod_model_score
+
+    if train_model_score>prod_model_score:
+        run_id = experiment_best["run_id"]
+else:
+    run_id = experiment_best["run_id"]
+
+# Registering the best model
+if run_id is not None:
+
+    model_uri = "runs:/{run_id}/{artifact_path}".format(
+        run_id=run_id,
+        artifact_path=artifact_path
+    )
+    model_details = mlflow.register_model(model_uri=model_uri, name=model_name)
+    wait_until_ready(model_details.name, model_details.version)
+    model_details = dict(model_details)
